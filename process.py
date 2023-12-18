620afe78a7cc443052121c52aba54d8136614c9f
Joseph Slack <josephdslack@gmail.com>
2023-12-17 17:55:50 -0800

    add process glcloud documents ai

---
+++ process.py
@@ -0,0 +1,8 @@
+from process import process_document
+
+process_document_sample(
+  project_id="675345247329",
+  location="us",
+  processor_id="346c58a850f0023e",
+  file_path="/path/to/local/pdf"
+)
