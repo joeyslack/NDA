d9fbea137e94e558c63352debac6850525045b32
Joseph Slack <josephdslack@gmail.com>
2024-04-28 17:09:54 -0700

    Cleanup

--- paula.py
+++ paula.py
@@ -1,9 +1,7 @@
 # Scraping for Paula.csv skincare stuff
-
 import time
 import re
 import random
-
 # import pandas as pd
 import undetected_chromedriver as uc
 from selenium.webdriver.remote.webdriver import By
@@ -12,7 +10,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 import psycopg2
 from config import load_config
-
 # options = uc.ChromeOptions()
 driver = uc.Chrome(headless=True,use_subprocess=True)
 
@@ -64,36 +61,4 @@ time.sleep(5)
 curr.close()
 conn.close()
 
-
-# input("Open the browser, do your full authentication, and press [ENTER] to continue....")
-
-# startRow = 0
-# listName = "Ninas List"
-# driver.get(url=f"https://www.harmonic.ai/")
-
-# # 2. Save results to table, paginate
-# while i <= 201:
-#   delay = 3
-#   wrapper = WebDriverWait(driver, delay).until(
-#     EC.presence_of_element_located((By.CSS_SELECTOR, '.cstm-table'))
-#   )
-
-#   time.sleep(3)
-
-#   df = driver.find_element(By.CSS_SELECTOR, '.jss31.cstm-table')
-#   dfs = pd.read_html(df.get_attribute("outerHTML"))
-#   df = dfs[0]
-#   df2 = df[['Rank','Company', 'City', 'Country', 'Funding', 'Industry', 'Employees', 'Revenue', 'Emp Growth %']]
-#   print(dfs)
-#   df2.to_csv('./output/ninaslist.csv', mode='a', index=False, header=False)
-#   # with pd.ExcelWriter('./output/growjo.xlsx', engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:  
-#     # df2.to_excel(writer, header=False, index=False, startrow=writer.sheets[listName].max_row)
-#   print (f"Finished page: {i}, Next is: {i+1}, Next record start @: {startRow}")
-  
-#   startRow += 51
-#   nextButton = driver.find_element(By.XPATH, "//li[@class='next']//a")
-#   driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-#   nextButton.click()
-  
-#   time.sleep(3)
-#   i = i + 1
+# Last ran to: Updated id = 644
